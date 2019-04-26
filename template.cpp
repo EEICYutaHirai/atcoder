9786fd4caac1f61c58634cdd1fa36ffd8b10ebe8
EEICYutaHirai <paperbackprogrammer@gmail.com>
2019-04-26 14:26:57 +0900

    add Imos2D

--- template.cpp
+++ template.cpp
@@ -181,6 +181,23 @@ class UnionFind
     }
 };
 
+template <class T>
+std::vector<std::vector<T>> Imos2D(const std::vector<std::vector<T>> &a)
+{
+    int h = a.size(), w = a[0].size();
+    std::vector<std::vector<T>> s(h + 1, std::vector<T>(w + 1, 0));
+    rep(i, h) rep(j, w) s[i + 1][j + 1] = a[i][j];
+    rep(i, h + 1) rep(j, w) s[i][j + 1] += s[i][j];
+    rep(i, h) rep(j, w + 1) s[i + 1][j] += s[i][j];
+    return s;
+}
+
+template <class T>
+int sum(const std::vector<std::vector<T>> &s, int i, int j, int h, int w)
+{
+    return s[i + h][j + w] - s[i][j + w] + s[i][j] - s[i + h][j];
+}
+
 signed main()
 {
 }
